1fdbe0f5669499c25895681b69546c0c6cd6addc
hailqueenflo <florencelim14@gmail.com>
2020-08-26 22:20:39 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,23 +3,40 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        String line = "null";
+        String[] commandList = new String[100];
+        int num = 0;
 
-        String line = in.nextLine();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
         // when input is not "bye"
         while (!(line.equals("bye"))) {
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
             line = in.nextLine();
-        }
 
-        // when input is "bye"
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+            // when input is "bye"
+            if(line.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+            }
+
+            else if(line.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < num; i++) {
+                    System.out.println("     " + (i + 1) + ". " + commandList[i]);
+                }
+                System.out.println("    ____________________________________________________________");
+            }
+            else {
+                commandList[num] = line;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     " + "added: " + line);
+                System.out.println("    ____________________________________________________________");
+                num++;
+            }
+        }
     }
 }
