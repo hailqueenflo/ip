0edfa9c1ac2f00afb1466249e5a76a62d11e2f61
hailqueenflo <florencelim14@gmail.com>
2020-08-27 12:35:24 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,41 +2,52 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String horLine = "    ____________________________________________________________";
+        String hello = horLine +
+                "\n     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                horLine;
+        String goodbye = horLine +
+                "\n     Bye. Hope to see you again soon!\n" + horLine;
+
         Scanner in = new Scanner(System.in);
         String line = "null";
-        String[] commandList = new String[100];
-        int num = 0;
+        Task[] taskList = new Task[100];
 
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    Hello! I'm Duke");
-        System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________________________________");
+        // print hello message
+        System.out.println(hello);
+
+        line = in.nextLine();
 
         // when input is not "bye"
         while (!(line.equals("bye"))) {
-            line = in.nextLine();
+            String[] words = line.split(" ");
 
-            // when input is "bye"
-            if(line.equals("bye")) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
-            }
-
-            else if(line.equals("list")) {
-                System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < num; i++) {
-                    System.out.println("     " + (i + 1) + ". " + commandList[i]);
+            if(line.equals("list")) {
+                System.out.println(horLine + "\n Here are the tasks in your list:");
+                for (int i = 0; i <Task.getNum(); i++) {
+                    System.out.println("     " + (i + 1) + ". " +
+                            taskList[i].getStatusIcon() +
+                            taskList[i].getDescription());
                 }
-                System.out.println("    ____________________________________________________________");
+                System.out.println(horLine);
+            }
+            else if(words[0].equals("done")) {
+                int id = Integer.parseInt(words[1]);
+                Task task = taskList[id-1];
+                task.markAsDone();
+                System.out.println(horLine +
+                        "\n     Nice! I've marked this task as done: \n" + "       " +
+                        task.getStatusIcon() + " " + task.getDescription() + "\n" + horLine);
             }
             else {
-                commandList[num] = line;
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     " + "added: " + line);
-                System.out.println("    ____________________________________________________________");
-                num++;
+                Task newTask = new Task(line);
+                taskList[newTask.getId()-1] = newTask;
+                System.out.println(horLine +
+                        "\n     added: " + line + '\n' + horLine);
             }
+            line = in.nextLine();
         }
+        System.out.println(goodbye);
     }
 }
