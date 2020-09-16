0371c15ce174eb34ec1fc2b02fb6ff07f65d87e5
hailqueenflo <florencelim14@gmail.com>
2020-09-16 22:22:34 +0800

    Duke Level-7: Save

--- src/main/java/FileAccess.java
+++ src/main/java/FileAccess.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 public class FileAccess {
     private static final String FILE_PATH = "./docs/duke.txt";
 
-    private TaskType taskType;
+    private final TaskType taskType;
 
     public FileAccess (TaskType list) {
         this.taskType = list;
     }
-    public void saveData() {
+    public void writeToFile () {
         try {
             FileWriter fw = new FileWriter(FILE_PATH);
 
@@ -25,5 +25,4 @@ public class FileAccess {
             e.printStackTrace();
         }
     }
-
 }
