06d614e6c2e4dc81d11ef5fbe73155d062c948bb
hailqueenflo <florencelim14@gmail.com>
2020-09-25 22:24:56 +0800

    Level-8: Dates and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,21 @@
 public class Event extends Task {
 
     protected String at;
+    protected String formattedAt;
 
     public Event(String description, String at) {
         super(description);
+        formattedAt = Parser.getFormattedDate(at.strip());
         this.at = at;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.at + ")";
+        return "[E]" + super.toString() + " (at: " + formattedAt + ")";
     }
 
     @Override
     public String writeToFile() {
-        return "E|" + isDone + "|" + this.description + "|" + this.at + "\n";
+        return "E|" + isDone + "|" + this.description + "|" + formattedAt + "\n";
     }
 }
